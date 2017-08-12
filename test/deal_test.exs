1ae393ead38f93ff207424f7493a5a1992cd4f7e
xvw <xavier.vdw@gmail.com>
2017-08-12 18:38:25 +0200

    Append tests for Deal

--- test/deal_test.exs
+++ test/deal_test.exs
@@ -11,17 +11,42 @@ defmodule DealTest do
   def h(x), do: {:ok, x+3}
   def i(x), do: x+6
 
-  test "Un test" do
+  test "Simple deal use case" do
+    result = Deal.with! do
+      f(1)
+      g()
+      h()
+    end
+    assert result == {:ok, 6}
+  end
 
+  test "Deal with unexcepted pattern" do
+    result = Deal.with! do
+      f(1)
+      i()
+      h()
+    end
+    assert result == 7
+  end
 
-    x = Deal.with! do: (f(1); f(); h())
+  test "Deal inlined" do
+    result = Deal.with! do: (f(1); g(); h())
+    assert result == {:ok, 6}
+  end
 
-    IO.inspect x
+  test "Deal inlined with unexcepted pattern" do
+    result = Deal.with! do: (f(1); i(); h())
+    assert result == 7
+  end
 
-    # assert x == {:ok, 6}
+  test "Deal with tuple" do
+    result = Deal.with! do: {f(1), g(), h()}
+    assert result == {:ok, 6}
+  end
 
-
-
+  test "Deal with tuple and unexcepted pattern" do
+    result = Deal.with! do: {f(1), i(), h()}
+    assert result == 7
   end
 
 
